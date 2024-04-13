bc44c567933675c1791a0e2356c14a541f1cf88c
Ayan Nair <ayannair@umich.edu>
2024-04-13 17:21:14 -0400

    camera worked

--- frontend/visuaiizeapp/src/visuaiizeapp/app.py
+++ frontend/visuaiizeapp/src/visuaiizeapp/app.py
@@ -4,15 +4,23 @@ Build the world around you for you
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
+from toga import Image, ImageView, Canvas
+# import pygame 
+# import pygame.camera 
+from PIL import Image as img
+from io import BytesIO
+import os
+import cv2
 
 
 class VisuAIizeApp(toga.App):
+    main_box = toga.Box()
     def startup(self):
         
         main_box = toga.Box()
         button = toga.Button(
             "START MY DAY",
-            on_press=self.take_picture,
+            on_press=self.save_picture,
             style=Pack(padding=5)
         )
 
@@ -23,21 +31,55 @@ class VisuAIizeApp(toga.App):
             )
         )
 
+        #my_image = toga.Image("filename.png")
+        #view = toga.ImageView(my_image)
+
         main_box.add(button)
+        #main_box.add(view)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
-    async def take_picture(self, widget, **kwargs):
-        photo = self.activate_camera
-        self.main_box.ImageView(
-            photo
-        )
-    async def activate_camera(self, widget, **kwargs):
+    async def save_picture(self, widget,**kwargs):
         self.camera.request_permission()
-        photo = await self.camera.take_photo()
-        return photo
-        
+        #photo = await self.camera.take_photo()
+        #photo_pil = photo.as_format(img.Image)
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        #photo_pil.save(dir_path+"/photos/photo.png", "PNG")
+        cap = cv2.VideoCapture(0)
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                print("dumb")
+                break
+            cv2.imwrite(dir_path+"/photos/newPhoto.png", frame)
+        # d = photo.data
+        # im = img.open(BytesIO(d))
+        # im.show()
+
+    # def activate_camera(self, widget, **kwargs):
+    #     #self.camera.request_permission()
+    #     #photo = self.camera.take_photo()
+    #     #return photo
+    #     pygame.camera.init() 
+  
+    #     # make the list of all available cameras 
+    #     camlist = pygame.camera.list_cameras() 
+  
+    #     # if camera is detected or not 
+    #     if camlist: 
+    #         # initializing the cam variable with default camera 
+    #         cam = pygame.camera.Camera(camlist[0], (640, 480)) 
+    #         # opening the camera 
+    #         cam.start() 
+    #         # capturing the single image 
+    #         image = cam.get_image() 
+    #         pygame.display.set_caption('image')
+    #         # saving the image 
+    #         pygame.image.save(image, "filename.jpg")
+  
+# if camera is not detected the moving to else part 
+                
 
 
 def main():
