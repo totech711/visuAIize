658926afb24273eb8dfb11f5d887dfaf7c8e3227
yp583 <pately583@gmail.com>
2024-04-14 04:19:26 -0400

    web app with django

--- webapp/visuAIizer/views.py
+++ webapp/visuAIizer/views.py
@@ -1,17 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.http import StreamingHttpResponse
+from django.views.decorators import gzip
 import cv2
 import os
 
-def index(request):
-    cap = cv2.VideoCapture(0)
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        resize = cv2.resize(frame, (320, 320))
-        cv2.imwrite(dir_path+"/photos/newPhoto.png", resize)
-    return HttpResponse("")
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@gzip.gzip_page
+def livefe(request):
+    try:
+        cam = VideoCamera()
+        return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:  # This is bad! replace it with proper handling
+        pass
