4a9e0062d17657a692b889b3374e1410c9315d76
Ayan Nair <ayannair@umich.edu>
2024-04-14 03:52:49 -0400

    reverted

--- frontend/visuaiizeapp/src/visuaiizeapp/app.py
+++ frontend/visuaiizeapp/src/visuaiizeapp/app.py
@@ -5,149 +5,160 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 from toga import Image, ImageView, Canvas
-# import pygame 
-# import pygame.camera 
+# import pygame
+# import pygame.camera
 import os
 import cv2
 from time import sleep
 from .geminidriver import *
+
 
 from datetime import datetime
 import threading
 from gtts import gTTS
 
 
+
+
 class VisuAIizeApp(toga.App):
-    main_box = toga.Box()
-    def startup(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        with open(dir_path+"/resources/API_KEY.txt","r") as f:
-            api_keys = f.read().split("\n")
-        key1 = os.environ["API_KEY1"]
-        key2 = os.environ["API_KEY2"]
-        self.ai = VideoGemini(api_keys=api_keys, verbose=True, delete=False)
-        main_box = toga.Box()
-        button = toga.Button(
-            "START MY DAY",
-            on_press=self.save_picture,
-            style=Pack(padding=5)
-        )
-
-        main_box.add(
-            toga.ImageView(
-                
-                style=Pack(flex=1, width=150),
-            )
-        )
-
-        self.starting = datetime.now()
-        self.cap = None
-
-        #my_image = toga.Image("filename.png")
-        #view = toga.ImageView(my_image)
-
-        main_box.add(button)
-        #main_box.add(view)
-
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
-        self.main_window.show()
-    def get_response_from_gemini(self, query):
-        print("arrived gemini")
-        
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        response = self.ai.get_response(query)
-        print("token count size: "+str(response.candidates[0].token_count))
-        if (response and response.candidates[0].token_count != 0):
-            tts = gTTS(text=response.text.replace("<None>", ""), lang='en', slow=False)
-
-            # Save the audio file
-            tts.save(dir_path + "/photos/tts.mp3")
-            # Play the audio file
-            aud_path = dir_path + "/photos/tts.mp3"
-            os.system(f"afplay -r 1.5 {aud_path}")
-    def uploading_video(self):
-        print("arrived upload")
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        for i in range(10):
-            ret, frame = self.cap.read()
-            if not ret:
-                break
-            resize = cv2.resize(frame, (320, 320))
-            cv2.imwrite(dir_path+"/photos/newPhoto.png", resize)
-            file = File(dir_path+"/photos/newPhoto.png", (str(datetime.now()-self.starting).split(".")[0][2:]))
-            self.ai.upload_frame(file)
-    async def save_picture(self, widget,**kwargs):
-        self.camera.request_permission()
-        dir_path = os.path.dirname(os.path.realpath(__file__))
+   main_box = toga.Box()
+   def startup(self):
+       dir_path = os.path.dirname(os.path.realpath(__file__))
+       with open(dir_path+"/resources/API_KEY.txt","r") as f:
+           api_keys = f.read().split("\n")
+       key1 = os.environ["API_KEY1"]
+       key2 = os.environ["API_KEY2"]
+       self.ai = VideoGemini(api_keys=api_keys, verbose=True, delete=False)
+       main_box = toga.Box()
+       button = toga.Button(
+           "START MY DAY",
+           on_press=self.save_picture,
+           style=Pack(padding=5)
+       )
 
 
-        self.cap = cv2.VideoCapture(0)
-        frames_captured = 0
-        
-        while self.cap.isOpened():
-            # if frames_captured==0:
-            #     print("before first upload")
-            #     self.uploading_video()
-            #     print("after first upload")
-
-            # task1 = threading.Thread(target=self.uploading_video, daemon=True)
-            # task2 = threading.Thread(target=self.get_response_from_gemini, args=(None, ), daemon=True)
-            # task1.start()
-            # task2.start()
-            # # if (frames_captured != 0 and frames_captured % 20 == 0):
-            # #     self.ai.increment_api_key()
-            # task1.join()
-            # task2.join()
-            self.uploading_video()
-            query = None if frames_captured == 0 else "Was there significant changes from the last video?"
-            self.get_response_from_gemini(None)
-            frames_captured += 10
+       main_box.add(
+           toga.ImageView(
+              
+               style=Pack(flex=1, width=150),
+           )
+       )
 
 
-        # while self.cap.isOpened():
-        #     ret, frame = self.cap.read()
-        #     if not ret:
-        #         break
-        #     resize = cv2.resize(frame, (38, 66))
-        #     cv2.imwrite(dir_path+"/photos/newPhoto.png", resize)
-        #     file = File(dir_path+"/photos/newPhoto.png", (str(datetime.now()-self.starting).split(".")[0][2:]))
-        #     self.ai.upload_frame(file)
-        #     frames_captured += 1
-        #     if (frames_captured % 10 == 0):
-        #         query = None if frames_captured == 0 else "Did anything change significantly?"
-        #         task = asyncio.create_task(self.get_response_from_gemini(query))
-                
-            #sleep(0.5)
-        # d = photo.data
-        # im = img.open(BytesIO(d))
-        # im.show()
+       self.starting = datetime.now()
+       self.cap = None
 
-    # def activate_camera(self, widget, **kwargs):
-    #     #self.camera.request_permission()
-    #     #photo = self.camera.take_photo()
-    #     #return photo
-    #     pygame.camera.init() 
-  
-    #     # make the list of all available cameras 
-    #     camlist = pygame.camera.list_cameras() 
-  
-    #     # if camera is detected or not 
-    #     if camlist: 
-    #         # initializing the cam variable with default camera 
-    #         cam = pygame.camera.Camera(camlist[0], (640, 480)) 
-    #         # opening the camera 
-    #         cam.start() 
-    #         # capturing the single image 
-    #         image = cam.get_image() 
-    #         pygame.display.set_caption('image')
-    #         # saving the image 
-    #         pygame.image.save(image, "filename.jpg")
-  
-# if camera is not detected the moving to else part 
-                
+
+       #my_image = toga.Image("filename.png")
+       #view = toga.ImageView(my_image)
+
+
+       main_box.add(button)
+       #main_box.add(view)
+
+
+       self.main_window = toga.MainWindow(title=self.formal_name)
+       self.main_window.content = main_box
+       self.main_window.show()
+   def get_response_from_gemini(self, query):
+       print("arrived gemini")
+       dir_path = os.path.dirname(os.path.realpath(__file__))
+       response = self.ai.get_response(query)
+
+
+       tts = gTTS(text=response.text.replace("<None>", ""), lang='en', slow=False)
+
+
+       # Save the audio file
+       tts.save(dir_path + "/photos/tts.mp3")
+       # Play the audio file
+       aud_path = dir_path + "/photos/tts.mp3"
+       os.system(f"afplay -r 1.5 {aud_path}")
+   def uploading_video(self):
+       print("arrived upload")
+       dir_path = os.path.dirname(os.path.realpath(__file__))
+       for i in range(10):
+           ret, frame = self.cap.read()
+           if not ret:
+               break
+           resize = cv2.resize(frame, (320, 320))
+           cv2.imwrite(dir_path+"/photos/newPhoto.png", resize)
+           file = File(dir_path+"/photos/newPhoto.png", (str(datetime.now()-self.starting).split(".")[0][2:]))
+           self.ai.upload_frame(file)
+   async def save_picture(self, widget,**kwargs):
+       self.camera.request_permission()
+       dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+
+
+       self.cap = cv2.VideoCapture(0)
+       frames_captured = 0
+      
+       while self.cap.isOpened():
+           if frames_captured<=15:
+               print("before first upload")
+               self.uploading_video()
+               print("after first upload")
+
+
+           task1 = threading.Thread(target=self.uploading_video, daemon=True)
+           task2 = threading.Thread(target=self.get_response_from_gemini, args=(None, ), daemon=True)
+           task1.start()
+           task2.start()
+           task1.join()
+           frames_captured += 10
+
+
+
+
+       # while self.cap.isOpened():
+       #     ret, frame = self.cap.read()
+       #     if not ret:
+       #         break
+       #     resize = cv2.resize(frame, (38, 66))
+       #     cv2.imwrite(dir_path+"/photos/newPhoto.png", resize)
+       #     file = File(dir_path+"/photos/newPhoto.png", (str(datetime.now()-self.starting).split(".")[0][2:]))
+       #     self.ai.upload_frame(file)
+       #     frames_captured += 1
+       #     if (frames_captured % 10 == 0):
+       #         query = None if frames_captured == 0 else "Did anything change significantly?"
+       #         task = asyncio.create_task(self.get_response_from_gemini(query))
+              
+           #sleep(0.5)
+       # d = photo.data
+       # im = img.open(BytesIO(d))
+       # im.show()
+
+
+   # def activate_camera(self, widget, **kwargs):
+   #     #self.camera.request_permission()
+   #     #photo = self.camera.take_photo()
+   #     #return photo
+   #     pygame.camera.init()
+    #     # make the list of all available cameras
+   #     camlist = pygame.camera.list_cameras()
+    #     # if camera is detected or not
+   #     if camlist:
+   #         # initializing the cam variable with default camera
+   #         cam = pygame.camera.Camera(camlist[0], (640, 480))
+   #         # opening the camera
+   #         cam.start()
+   #         # capturing the single image
+   #         image = cam.get_image()
+   #         pygame.display.set_caption('image')
+   #         # saving the image
+   #         pygame.image.save(image, "filename.jpg")
+ # if camera is not detected the moving to else part
+              
+
+
 
 
 def main():
-    return VisuAIizeApp()
+   return VisuAIizeApp()
+
+
+
+
 
--- frontend/visuaiizeapp/src/visuaiizeapp/geminidriver.py
+++ frontend/visuaiizeapp/src/visuaiizeapp/geminidriver.py
@@ -1,104 +1,135 @@
 import os
 import google.generativeai as genai
 
+
 import cv2
 import os
 import shutil
 from gtts import gTTS
 #SYSTEM_PROMPT = "Imagine the video given is from your perspective. Describe the scene, including the people in the scene, the objects in the scene, and anything interesting that happens. Make sure to note where objects and people are relative to you"
 
-SYSTEM_PROMPT = "You are a viewing assistant for a blind person. Your job is to be their eyes, You will be given videos that represent the user's POV from the last 5 seconds. Provide a concise description, prioritizing nearby people, obstacles within a few steps, moving objects approaching us, and relevant environmental changes. Keep descriptions to one-two phrases.  Only provide descriptions when there are notable changes in the scene otherwise output <None> when you have already described the scene significantly. If anything is in reference to the camera, refer to the camera as 'you'"
+
+SYSTEM_PROMPT = "You are a viewing assistant for a blind person. Your job is to be their eyes, Provide a concise description, prioritizing nearby people, obstacles within a few steps, moving objects approaching us, and relevant environmental changes. Keep descriptions to one-two phrases and only update when there are changes or upon request.  Output <None> when you have already described the scene significantly. Summarize only the frames provided in 10 words maximum only. If anything is in reference to the camera, refer to the camera as 'you'"
+
 
 class File:
-    def __init__(self, file_path: str, timestamp: str, display_name: str = None):
-        self.file_path = file_path
-        if display_name:
-            self.display_name = display_name
-        self.timestamp = timestamp
-    def set_response(self, response):
-        self.response = response
+   def __init__(self, file_path: str, timestamp: str, display_name: str = None):
+       self.file_path = file_path
+       if display_name:
+           self.display_name = display_name
+       self.timestamp = timestamp
+   def set_response(self, response):
+       self.response = response
+
 
 class VideoGemini():
-    def __init__(self, api_keys: list[str], verbose: bool = False, delete: bool = True):
-        #api key switching logic
-        self.api_keys = api_keys
-        self.api_key_idx = 0
-        self.calls_this_min = 0
+   def __init__(self, api_keys: list[str], verbose: bool = False, delete: bool = True):
+       #api key switching logic
+       self.api_keys = api_keys
+       self.api_key_idx = 0
+       self.calls_this_min = 0
 
-        genai.configure(api_key=api_keys[self.api_key_idx])
 
-        self.model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest",  system_instruction=SYSTEM_PROMPT, 
-        #         generation_config=genai.GenerationConfig(
-        #     max_output_tokens = 100,
-        #     temperature = 1.0
-        # )
-        )
+       genai.configure(api_key=api_keys[self.api_key_idx])
 
-        self.frames = []
 
-        self.verbose = verbose
+       self.model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest",  system_instruction=SYSTEM_PROMPT)
 
-        self.delete = delete
 
-        self.chat = self.model.start_chat(history=[])
+       self.frames = []
 
-    
-    def upload_frame(self, file: File):
-        if (self.verbose):
-            print(file.file_path)
-            print(f'Uploading: {file.file_path}...')
-        response = genai.upload_file(path=file.file_path)
-        file.set_response(response)
-        if (self.verbose):
-            print(f"Completed file upload")
-            if (self.delete):
-                print(f"Deleting local file at {file.file_path}")
-        if (self.delete):
-            os.remove(file.file_path)
-            file.file_path = ""
-        self.frames.append(file)
 
-    def _build_request(self, query:str = None):
-        request = []
-        if (query):
-            request.append(query)
-        for frame in self.frames:
-            request.append(frame.timestamp)
-            request.append(frame.response)
-        return request
-    
-    def increment_api_key(self):
-        self.api_key_idx += 1
-        self.api_key_idx = self.api_key_idx % len(self.api_keys)
-        genai.configure(api_key=self.api_keys[self.api_key_idx])
+       self.verbose = verbose
 
-    def get_response(self, query:str = None):
-        if self.calls_this_min >= 2:
-            self.api_key_idx += 1
-            self.api_key_idx = self.api_key_idx % len(self.api_keys)
-            genai.configure(api_key=self.api_keys[self.api_key_idx])
-            self.calls_this_min = 0
-        self.calls_this_min += 1
-        print(f"gemini call api key is: {self.api_key_idx}")
 
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-            
-        # Make the LLM request.
-        request = self._build_request(query)
-        response = self.chat.send_message(request)
+       self.delete = delete
 
-        return response
 
-    def _delete_frames(self):
-        if (self.verbose):
-            print(f'Deleting {len(self.frames)} images. This might take a bit...')
-        for frame in self.frames:
-            print("here")
-            genai.delete_file(frame.response.name)
-            print('here2')
-            if (self.verbose):
-                print(f'Deleted {frame.file_path} at URI {frame.response.uri}')
-        if (self.verbose):
-            print(f"Completed deleting files!\n\nDeleted: {len(self.frames)} files")
-    def __del__(self):
-        self._delete_frames()
+       self.chat = self.model.start_chat(history=[])
+
+
+      
+   async def async_upload_frame(self, file: File):
+       if (self.verbose):
+           print(file.file_path)
+           print(f'Uploading: {file.file_path}...')
+       response = genai.upload_file(path=file.file_path)
+       file.set_response(response)
+       if (self.verbose):
+           print(f"Completed file upload")
+           if (self.delete):
+               print(f"Deleting local file at {file.file_path}")
+       if (self.delete):
+           os.remove(file.file_path)
+           file.file_path = ""
+       self.frames.append(file)
+
+
+   def upload_frame(self, file: File):
+       if (self.verbose):
+           print(file.file_path)
+           print(f'Uploading: {file.file_path}...')
+       response = genai.upload_file(path=file.file_path)
+       file.set_response(response)
+       if (self.verbose):
+           print(f"Completed file upload")
+           if (self.delete):
+               print(f"Deleting local file at {file.file_path}")
+       if (self.delete):
+           os.remove(file.file_path)
+           file.file_path = ""
+       self.frames.append(file)
+
+
+   def _build_request(self, query:str = None):
+       request = []
+       if (query):
+           request.append(query)
+       for frame in self.frames:
+           request.append(frame.timestamp)
+           request.append(frame.response)
+       return request
+  
+   async def async_get_response(self, query:str = None):
+       if (self.calls_this_min >= 2):
+           self.api_key_idx += 1
+           self.api_key_idx = self.api_key_idx % len(self.api_keys)
+           self.calls_this_min = 0
+       self.calls_this_min += 1
+          
+       # Make the LLM request.
+       request = self._build_request(query)
+       response = self.chat.send_message(request)
+       self.frames = []
+       return (response.text.replace("<None>", ""))
+
+
+   def get_response(self, query:str = None):
+       print(f"gemini call api key is: {self.api_key_idx}")
+       if (self.calls_this_min >= 2):
+           self.api_key_idx += 1
+           self.api_key_idx = self.api_key_idx % len(self.api_keys)
+           self.calls_this_min = 0
+       self.calls_this_min += 1
+       dir_path = os.path.dirname(os.path.realpath(__file__))
+          
+       # Make the LLM request.
+       request = self._build_request(query)
+       response = self.chat.send_message(request)
+       return response
+
+
+   def _delete_frames(self):
+       if (self.verbose):
+           print(f'Deleting {len(self.frames)} images. This might take a bit...')
+       for frame in self.frames:
+           print("here")
+           genai.delete_file(frame.response.name)
+           print('here2')
+           if (self.verbose):
+               print(f'Deleted {frame.file_path} at URI {frame.response.uri}')
+       if (self.verbose):
+           print(f"Completed deleting files!\n\nDeleted: {len(self.frames)} files")
+   def __del__(self):
+       self._delete_frames()
+
